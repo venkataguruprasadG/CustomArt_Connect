f0dd0017ca2321867f1991c26ea1d8f6254c75e3
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-04 12:23:19 +0530

    Created the Form and input in login Page.

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,40 @@
+import "./Login.css";
+import { useState } from "react";
+
+
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Email: ", email);
+        console.log("Password: ", password);
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
