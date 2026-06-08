2133f1b117198dfc99cf7538144c75b58d27bf17
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-08 16:33:39 +0530

    Artist Card have been designed.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,43 @@
 import HeroSection from "../components/HeroSection";
+import ArtistCard from "../components/ArtistCard";
+
 function Home() {
+    const artists = [
+        {
+            id: 1,
+            name: "Rahul Sharma",
+            specialization: "Digital Artist",
+            image: "https://picsum.photos/300?1"
+        },
+
+        {
+            id: 2,
+            name: "Priya Reddy",
+            specialization: "Portrait Artist",
+            image: "https://picsum.photos/300?2"
+        },
+
+        {
+            id: 3,
+            name: "Arjun Kumar",
+            specialization: "Sketch Artist",
+            image: "https://picsum.photos/300?3"
+        }
+    ];
     return (
         <>
             <HeroSection />
+
+            <div className="artist-container">
+                {artists.map(artist => (
+                    <ArtistCard
+                        key={artist.id}
+                        name={artist.name}
+                        description={artist.specialization}
+                        image={artist.image}
+                    />
+                ))}
+            </div>
         </>
     );
 }
---
+++ frontend/src/components/ArtistCard.jsx
@@ -0,0 +1,18 @@
+function ArtistCard(props) {
+    return (
+        <div className="artist-card">
+            <img
+                src={props.image}
+                alt={props.name}
+            />
+
+            <h3>{props.name}</h3>
+
+            <p>{props.description}</p>
+
+            <button>View Profile</button>
+        </div>
+    );
+}
+
+export default ArtistCard;
