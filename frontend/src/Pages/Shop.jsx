67cc760eafa94929ebd3609a5fe5a0cf85c42030
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-13 22:05:03 +0530

    added footer to the Shop page.

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,4 +1,6 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 function Shop() {
     return (
         <>
@@ -9,6 +11,7 @@ function Shop() {
 
                 </div>
             </section>
+            <Footer />
         </>
     );
 }
