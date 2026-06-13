2a4a74f072d879a3065be63d1a7d92005f5ed08d
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-13 17:00:47 +0530

    added navbar to the Shop Page.

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,6 +1,8 @@
+import Navbar from "../components/Navbar"
 function Shop() {
     return (
         <>
+            <Navbar />
             <section className="shop-container">
 
             </section>
